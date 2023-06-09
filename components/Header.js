70bbdcb2c282308a204c67e86b62886a580b45b5
AnwarK.code <115826335+AnwarKXC@users.noreply.github.com>
2023-06-09 23:12:08 +0300

    first

---
+++ components/Header.js
@@ -0,0 +1,57 @@
+import Link from "next/link";
+
+export default function Header () {
+   return (
+      <nav className="bg-zinc-800 p-1">
+         <div className="p-2 flex justify-between">
+            <div className=" flex gap-4 text-lg items-center pb-4">flix scans <span className=" bg-zinc-600 px-2 py-1 rounded-lg text-xs">WEBTOONS</span><span className="text-xs">LIBERARY</span>
+            </div>
+            <div className="flex items-center">
+               <span className="p-1 px-3 hover:bg-zinc-600 rounded-md "><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 00-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 01-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 003 15h-.75M15 10.5a3 3 0 11-6 0 3 3 0 016 0zm3 0h.008v.008H18V10.5zm-12 0h.008v.008H6V10.5z" />
+               </svg>
+
+               </span>
+               <span className="p-1 px-3 hover:bg-zinc-600 rounded-md"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+               </svg>
+
+               </span>
+               <span className="p-1 px-3 hover:bg-zinc-600 rounded-md"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M15.362 5.214A8.252 8.252 0 0112 21 8.25 8.25 0 016.038 7.048 8.287 8.287 0 009 9.6a8.983 8.983 0 013.361-6.867 8.21 8.21 0 003 2.48z" />
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M12 18a3.75 3.75 0 00.495-7.467 5.99 5.99 0 00-1.925 3.546 5.974 5.974 0 01-2.133-1A3.75 3.75 0 0012 18z" />
+               </svg>
+
+               </span>
+               <span className="p-1 px-3 hover:bg-zinc-600 rounded-md"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+               </svg>
+
+               </span>
+            </div>
+         </div>
+         <ul className="flex  text-xs">
+            <Link className="p-1 px-3 hover:bg-zinc-600 rounded-md flex gap-1" href={ "/" }> <span> <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+               <path stroke-linecap="round" stroke-linejoin="round flex gap-1" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+            </svg>
+            </span>  HOME</Link>
+            <Link className="p-1 px-3 hover:bg-zinc-600 rounded-md flex gap-1 items-center justify-center  " href={ "/" }> <span> <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+               <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+            </svg>
+            </span> ROMANCE</Link>
+            <Link className="p-1 px-3 hover:bg-zinc-600 rounded-md flex gap-1 items-center justify-center " href={ "/" }> <span> <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+               <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 13.5l10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75z" />
+            </svg>
+            </span> ACTION</Link>
+            <Link className="p-1 px-3 hover:bg-zinc-600 rounded-md flex gap-1 items-center justify-center " href={ "/" }> <span><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+               <path stroke-linecap="round" stroke-linejoin="round" d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z" />
+            </svg>
+            </span> FANTASY</Link>
+            <Link className="p-1 px-3 hover:bg-zinc-600 rounded-md flex gap-1 items-center justify-center " href={ "/" }> <span><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+               <path stroke-linecap="round" stroke-linejoin="round" d="M6.115 5.19l.319 1.913A6 6 0 008.11 10.36L9.75 12l-.387.775c-.217.433-.132.956.21 1.298l1.348 1.348c.21.21.329.497.329.795v1.089c0 .426.24.815.622 1.006l.153.076c.433.217.956.132 1.298-.21l.723-.723a8.7 8.7 0 002.288-4.042 1.087 1.087 0 00-.358-1.099l-1.33-1.108c-.251-.21-.582-.299-.905-.245l-1.17.195a1.125 1.125 0 01-.98-.314l-.295-.295a1.125 1.125 0 010-1.591l.13-.132a1.125 1.125 0 011.3-.21l.603.302a.809.809 0 001.086-1.086L14.25 7.5l1.256-.837a4.5 4.5 0 001.528-1.732l.146-.292M6.115 5.19A9 9 0 1017.18 4.64M6.115 5.19A8.965 8.965 0 0112 3c1.929 0 3.716.607 5.18 1.64" />
+            </svg>
+            </span> DRAMA</Link>
+         </ul>
+      </nav>
+   )
+}
---
+++ components/Slides.js
@@ -0,0 +1,91 @@
+import React, { useEffect, useState } from 'react';
+import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs';
+import { RxDotFilled } from 'react-icons/rx';
+
+export default function Slides () {
+
+
+   const slides = [
+      {
+         url: 'https://media.flixscans.com/sliders/bbe600ad-74fd-464e-b78a-61e15ae0930e/6EdE7mzjbZxCSqRIjYB82PltXsIAPi-metaSTZkWXRsclBzTGN1b0d2aGZ2NFNTNlUyWFVBRTd4LW1ldGFVbVZoY0dWeUxXOW1MWFJvWlMxRWNtbG1kR2x1WnkxTmIyOXVMbmRsWW5BPS0ud2VicA==-.webp',
+      },
+      {
+         url: 'https://media.flixscans.com/sliders/e6b1ed28-d8d4-4c88-b115-8fe64a973c15/sAKVnp4bgkwD8lUnWbqiWDM50JfKlO-metaRG9jdG9y4oCZcyBSZWJpcnRoLmpwZw==-.webp',
+      },
+      {
+         url: 'https://media.flixscans.com/sliders/b238e61a-ba35-4be7-aa06-43a3718abfaa/FAgDaI9tXCd5Iq2fOmgc9HLzpM4Swn-metaRGVhdGggSXMgdGhlIE9ubHkgRW5kaW5nIGZvciB0aGUgVmlsbGFpbmVzczEuanBn-.webp',
+      },
+
+      {
+         url: 'https://media.flixscans.com/sliders/cc1c464d-6355-4ee5-8a5b-7af0a2d8b187/SKFUudVeLQPBqssriaOFKDWvFSsP4U-metaVGhlIFZpbGxhaW7igJlzIFNhdmlvci5qcGc=-.webp',
+      },
+      {
+         url: 'https://media.flixscans.com/sliders/606c92bf-e328-456f-be45-8d05f07abd83/zXAOLwAdNqN3ocBaJ2oOZiEJbWy8Co-metaSS1TdG9sZS10aGUtTnVtYmVyLU9uZS1SYW5rZXLigJlzLVNvdWwud2VicA==-.webp',
+      },
+      {
+         url: 'https://media.flixscans.com/sliders/3fecc32c-496b-4ec6-b83b-fc20ae74b6c5/70M6406tIoIvDKyqrJlgiNrQrUPOHx-metaTWFycmlhZ2Ugb2YgQ29udmVuaWVuY2UuanBn-.webp',
+      },
+      {
+         url: 'https://media.flixscans.com/sliders/36121425-82bc-47f8-9e52-18450c9205a6/LQjyTjJDbbiPQAVhUR2w1bvOB0Tsyh-metac3F1YXJlX2FmODgxYjgxLWQ3ZWItNGMwMi1iYjA1LTc3MzYxMjVmZWE0YyAoMSkuanBn-.webp',
+      },
+      {
+         url: 'https://media.flixscans.com/sliders/350d1bce-9fd6-433d-894a-cd4383b0b0fe/jeCuGQQTf0RycBE7l9kkkCPTcSYMMr-metaVGhlIEdyZWF0IE1hZ2UgUmV0dXJucyBBZnRlciA0MDAwIFllYXJzMS5qcGc=-.webp',
+      },
+   ];
+
+   const [ currentIndex, setCurrentIndex ] = useState( 0 );
+   const isFirstSlide = currentIndex === 0;
+   const isLastSlide = currentIndex === slides.length - 1;
+
+   const prevSlide = () => {
+      const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+      setCurrentIndex( newIndex );
+   };
+
+   const nextSlide = () => {
+      const newIndex = isLastSlide ? 0 : currentIndex + 1;
+      setCurrentIndex( newIndex );
+   };
+   const preSlide = isLastSlide ? 0 : currentIndex + 1
+   const postSlide = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+   
+   let slideInterval;
+   const intervalTime = 3000;
+
+   function auto () {
+   slideInterval=setInterval(nextSlide,intervalTime)
+}
+   useEffect( () => {
+   auto()
+},[currentIndex])
+   
+   
+   return (
+      <main className='max-w-[1400px] h-[500px] w-full m-auto py-16 px-8 relative group flex items-center'>
+         <div style={ { backgroundImage: `url(${ slides[ preSlide ].url })` } }
+            className=' w-2/5 h-5/6 rounded-md bg-center bg-cover duration-500 grayscale'>
+
+         </div>
+         {/* Left Arrow */ }
+         <div className=' absolute top-[50%] -translate-x-0 translate-y-[-50%] -left-10 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+            <BsChevronCompactLeft onClick={ prevSlide } size={ 30 } />
+         </div>
+
+         <div
+            style={ { backgroundImage: `url(${ slides[ currentIndex ].url })` } }
+            className='w-3/5 h-full mx-10 rounded-md bg-center bg-cover duration-500'
+         ></div>
+
+         {/* Right Arrow */ }
+         <div className=' absolute top-[50%] -translate-x-0 translate-y-[-50%] -right-10 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+            <BsChevronCompactRight onClick={ nextSlide } size={ 30 } />
+         </div>
+
+         <div style={ { backgroundImage: `url(${ slides[ postSlide ].url })` } }
+            className=' w-2/5  h-5/6 rounded-md bg-center bg-cover duration-500 grayscale'>
+
+         </div>
+      </main>
+   );
+}
+
